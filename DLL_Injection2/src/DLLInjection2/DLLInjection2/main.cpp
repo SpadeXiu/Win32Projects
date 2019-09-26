8f57f5ca1b60dd5d235d538a928e553e2383e341
l-iberty <2593093242@qq.com>
2019-09-26 22:54:18 +0800

    update

--- DLL_Injection2/src/DLLInjection2/DLLInjection2/main.cpp
+++ DLL_Injection2/src/DLLInjection2/DLLInjection2/main.cpp
@@ -1,9 +1,26 @@
 ﻿#include <Windows.h>
+#include <tchar.h>
 #include <iostream>
 #include "pe_file_helper.h"
 
-int main()
+//#define DEBUG
+
+int _tmain(int argc, wchar_t **argv)
 {
-	PeFileHelper pe_file(TEXT("testApp.exe"));
-	pe_file.InjectDll("testdll.dll", "dummy");
+#ifdef DEBUG
+	PeFileHelper pe_file_helper(TEXT("testApp.exe"));
+	pe_file_helper.InjectDll("testdll.dll", "dummy");
+#else
+	if (argc != 4) {
+		printf("usage: %ws <pe_file_name> <dll_name> <dll_export_proc_name>\n", argv[0]);
+		return 1;
+	}
+
+	PeFileHelper pe_file_helper(argv[1]);
+	char dll_name[MAX_PATH], proc_name[32];
+	sprintf(dll_name, "%ws", argv[2]);
+	sprintf(proc_name, "%ws", argv[3]);
+	pe_file_helper.InjectDll(dll_name, proc_name);
+
+#endif
 }
